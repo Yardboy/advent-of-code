86dfdc7e4525acc88ccc70c96d7739c8774c3baf
Cayce Balara <caycebalara@gmail.com>
2019-12-10 17:25:55 -0500

    Refactor day 10

--- 2019/puzzle10a/solution.rb
+++ 2019/puzzle10a/solution.rb
@@ -1,46 +1,69 @@
 #!/usr/local/bin/ruby
 require '../solution2019'
 
-class Solution < Solution2019
+Asteroid = Struct.new(:x, :y) do
+  def before?(station)
+    y < station.y || (y == station.y && x < station.x)
+  end
+end
+
+Path = Struct.new(:station, :asteroid) do
+  def key
+    [slope, quadrant]
+  end
+
   private
 
-  def process_input
-    @answer = [nil, 0]
-    @asteroids.each do |asteroid|
-      @slopes = {}
-      determine_slopes(asteroid)
-      @answer = [asteroid, @slopes.size] if @slopes.size > @answer[1]
-    end
+  def deltax
+    station.x - asteroid.x
   end
 
-  def determine_slopes(asteroid)
-    (@asteroids - [asteroid]).each do |station|
-      slope = slope(station, asteroid)
-      quadrant = quadrants[signs(station, asteroid)]
-      @slopes[[slope, quadrant]] ||= []
-      @slopes[[slope, quadrant]] << station
-    end
+  def deltay
+    station.y - asteroid.y
   end
 
-  def slope(station, asteroid)
-    ((station[0] - asteroid[0]).to_f / (station[1] - asteroid[1]).to_f).round(5)
+  def slope
+    (deltay.to_f / deltax.to_f * -1.0).round(5)
   end
 
-  def signs(station, asteroid)
-    "#{sign_of(station[0] - asteroid[0])}#{sign_of(station[1] - asteroid[1])}"
+  def signs
+    [deltay, deltax].map { |value| '++-'[value <=> 0] }.join('')
   end
 
-  def sign_of(value)
-    '++-'[value <=> 0]
-  end
-
-  def quadrants
+  def quadrant
     {
       '--' => 0,
       '+-' => 1,
       '++' => 2,
       '-+' => 3
-    }
+    }[signs]
+  end
+end
+
+class Solution < Solution2019
+  private
+
+  def process_input
+    @answer = [nil, 0]
+    @answer = find_best_station
+    @answer.delete_at(1)
+  end
+
+  def find_best_station
+    result = [nil, [], 0]
+    @asteroids.each do |asteroid|
+      paths = determine_paths(asteroid)
+      result = [asteroid, paths, paths.size] if paths.size > result[2]
+    end
+    result
+  end
+
+  def determine_paths(asteroid)
+    (@asteroids - [asteroid]).each_with_object({}) do |station, hsh|
+      path = Path.new(station, asteroid)
+      hsh[path.key] ||= []
+      hsh[path.key] << station
+    end
   end
 
   # override
@@ -49,7 +72,7 @@ class Solution < Solution2019
     @asteroids = []
     @input.each_with_index do |line, row|
       line.split('').each_with_index do |char, col|
-        @asteroids << [col, row] if char == '#'
+        @asteroids << Asteroid.new(col, row) if char == '#'
       end
     end
   end
--- 2019/puzzle10b/solution.rb
+++ 2019/puzzle10b/solution.rb
@@ -1,70 +1,86 @@
 #!/usr/local/bin/ruby
 require '../solution2019'
 
+Asteroid = Struct.new(:x, :y) do
+  def before?(station)
+    y < station.y || (y == station.y && x < station.x)
+  end
+end
+
+Path = Struct.new(:station, :asteroid) do
+  def key
+    [slope, quadrant]
+  end
+
+  private
+
+  def deltax
+    station.x - asteroid.x
+  end
+
+  def deltay
+    station.y - asteroid.y
+  end
+
+  def slope
+    (deltay.to_f / deltax.to_f * -1.0).round(5)
+  end
+
+  def signs
+    [deltay, deltax].map { |value| '++-'[value <=> 0] }.join('')
+  end
+
+  def quadrant
+    {
+      '--' => 0,
+      '+-' => 1,
+      '++' => 2,
+      '-+' => 3
+    }[signs]
+  end
+end
+
 class Solution < Solution2019
   private
 
   def process_input
     @answer = [nil, 0]
-    @slopes = {}
-    @asteroids.each do |asteroid|
-      @slopes = {}
-      determine_slopes(asteroid)
-      @answer = [asteroid, @slopes.size, @slopes] if @slopes.size > @answer[1]
-    end
-    sort_slopes
+    @station, @paths, @answer = find_best_station
     @answer = vaporized[199]
     @answer = @answer[0] * 100 + @answer[1]
   end
 
   def vaporized
+    sort_paths
     vaporized = []
-    until @slopes.all? { |_, asteroids| asteroids.empty? } do
-      @slopes.each do |_, asteroids|
+    until @paths.all? { |_, asteroids| asteroids.empty? } do
+      @paths.each do |_, asteroids|
         vaporized << asteroids.shift unless asteroids.empty?
       end
     end
     vaporized
   end
 
-  def sort_slopes
-    @slopes = @answer[2]
-    @slopes.each { |_, asteroids| asteroids.reverse! if before_station?(asteroids.first) }
-    @slopes = @slopes.to_a.sort_by { |(slope, quadrant), _| [quadrant, slope] }.reverse
-  end
-
-  def before_station?(asteroid)
-    asteroid[1] < @answer.first[1] || (asteroid[1] == @answer.first[1] && asteroid[0] < @answer.first[0])
-  end
-
-  def determine_slopes(asteroid)
-    (@asteroids - [asteroid]).each do |station|
-      slope = slope(station, asteroid)
-      quadrant = quadrants[signs(station, asteroid)]
-      @slopes[[slope, quadrant]] ||= []
-      @slopes[[slope, quadrant]] << station
+  def find_best_station
+    result = [nil, [], 0]
+    @asteroids.each do |asteroid|
+      paths = determine_paths(asteroid)
+      result = [asteroid, paths, paths.size] if paths.size > result[2]
     end
+    result
   end
 
-  def slope(station, asteroid)
-    ((station[1] - asteroid[1]).to_f / (station[0] - asteroid[0]).to_f * -1.0).round(5)
+  def sort_paths
+    @paths.each { |_, asteroids| asteroids.reverse! if asteroids.first.before?(@station) }
+    @paths = @paths.to_a.sort_by { |(slope, quadrant), _| [quadrant, slope] }.reverse
   end
 
-  def signs(station, asteroid)
-    "#{sign_of(station[1] - asteroid[1])}#{sign_of(station[0] - asteroid[0])}"
-  end
-
-  def sign_of(value)
-    '++-'[value <=> 0]
-  end
-
-  def quadrants
-    {
-      '--' => 0,
-      '+-' => 1,
-      '++' => 2,
-      '-+' => 3
-    }
+  def determine_paths(asteroid)
+    (@asteroids - [asteroid]).each_with_object({}) do |station, hsh|
+      path = Path.new(station, asteroid)
+      hsh[path.key] ||= []
+      hsh[path.key] << station
+    end
   end
 
   # override
@@ -73,7 +89,7 @@ class Solution < Solution2019
     @asteroids = []
     @input.each_with_index do |line, row|
       line.split('').each_with_index do |char, col|
-        @asteroids << [col, row] if char == '#'
+        @asteroids << Asteroid.new(col, row) if char == '#'
       end
     end
   end
